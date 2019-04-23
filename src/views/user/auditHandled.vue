da948dd1c43c5548d23c9a1df483d87b49be2528
huchao <1396953791@qq.com>
2019-04-23 14:15:07 +0800

    update

--- src/views/user/auditHandled.vue
+++ src/views/user/auditHandled.vue
@@ -59,10 +59,6 @@
             <el-form-item label="当前住址">
                 <el-input disabled="disable" v-model="form.name"></el-input>
             </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="onSubmit">立即创建</el-button>
-                <el-button>取消</el-button>
-            </el-form-item>
         </el-form>
     </el-dialog>
 </div>
